4b5e8e9bd07108f41a0ef27238ab8c553de11646
SamiaAurin <aurinsamia@gmail.com>
2025-01-13 19:03:05 +0600

    # Info   - `dummy.html` : Add HTML of filter button’s modal.   - `rentbyowner.css` : Add CSS for filter button’s modal   - `filter-sort.js` : Add code for open and close the modal.

--- static/js/filter-sort.js
+++ static/js/filter-sort.js
@@ -14,3 +14,43 @@ document.addEventListener("DOMContentLoaded", function() {
         sortBtn.classList.toggle('active');
     });
 });
+
+// Calendar open after Date Button Clicked
+document.addEventListener("DOMContentLoaded", function() {
+    const dateBtn = document.getElementById('filter-date-btn');
+    const datePicker = document.getElementById('js-date-picker');
+
+    dateBtn.addEventListener('click', function() {
+        datePicker.style.display = 'block';
+    });
+});
+
+// JS for Filter Modal 
+document.addEventListener("DOMContentLoaded", function() {
+    const dateBtn = document.getElementById('filter-date-btn');
+    const priceBtn = document.getElementById('filter-price-btn');
+    const guestsBtn = document.getElementById('filter-guests-btn');
+    const moreFiltersBtn = document.getElementById('filter-more-btn');
+    const filterModal = document.getElementById('js-filter-modal');
+    const closeBtns = document.getElementById('js-filter-close');
+
+    // Function to display the modal
+    function showModal() {
+        filterModal.style.display = 'block';
+    }
+
+    // Attach click event listeners to each button
+    dateBtn.addEventListener('click', showModal);
+    priceBtn.addEventListener('click', showModal);
+    guestsBtn.addEventListener('click', showModal);
+    moreFiltersBtn.addEventListener('click', showModal);
+
+    // Function to hide the modal
+    function hideModal() {
+        filterModal.style.display = 'none';
+    }
+
+    // Attach click event listeners to close buttons
+    closeBtns.addEventListener('click', hideModal);
+    
+});
